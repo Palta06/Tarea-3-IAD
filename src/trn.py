c8db6d79c093d42452bc69488515e9f9f5d9a506
Mathias <matiasfuentes.mf09@gmail.com>
2025-06-23 22:21:21 -0400

    carpetas

---
+++ src/trn.py
@@ -0,0 +1,15 @@
+# Training of RRK-Pinv
+
+import numpy as np
+from  utility import *
+
+
+# Main program
+def main():        
+    # Step 1: Parameters
+    X,Y,Sigma2,Lambd = load_data_csv('dtrain.csv','config.csv')
+#
+    
+
+if __name__ == "__main__":
+    main()
---
+++ src/utility.py
@@ -0,0 +1,23 @@
+
+import pandas as pd
+import numpy  as np
+
+# Data load from csv
+def load_data_csv(fname):    
+    return
+
+# Create Gaussian-kernel matrix
+def kernel_mtx(X1,X2,sigma2):
+    return 
+
+#  Estimating Coefficient  for KKR
+def krr_coeff(K,y,lamb):    
+    return beta
+# calculate measures
+def metricas(yv,zv):    
+    return()
+
+#Confusion matrix
+def confusion_mtx(yv,zv):    
+    return(cm)
+
