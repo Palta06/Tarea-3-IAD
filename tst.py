bcabe383e9451f22cd3432da6d0901c72291f580
Palta06 <bastian.guerra.v@mail.pucv.cl>
2025-06-23 22:15:13 -0400

    Add files via upload

---
+++ tst.py
@@ -0,0 +1,13 @@
+# Testing of RRK for IDS
+
+import numpy as np
+from  utility import *
+
+
+# Main program
+def main():    
+    # Step 1: Parameters
+    
+
+if __name__ == "__main__":
+    main()
